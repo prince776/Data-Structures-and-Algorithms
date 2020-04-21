7bb4ae80250e375289511435f8b89eaf76590805
prince776 <princemit776@gmail.com>
2020-04-21 16:01:12 +0530

    Added undirected graph with BFS and DFS traversals.

---
+++ DataStructures/Graphs/graphs_adjacency_list.cpp
@@ -0,0 +1,191 @@
+// Implements Graph Data Structure using adjacency list.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Graph
+{
+	int V;
+	unordered_set<int>* adjList;
+	bool *marked;
+	int *edgeTo;
+	int searchSrc;
+
+	Graph(int V);
+
+	~Graph()
+	{
+		delete[] adjList;
+		delete[] marked;
+		delete[] edgeTo;
+	}
+
+	void addEdge(int src, int dest);
+
+	void printGraph();
+
+	void searchEdge(int src, int dest);
+
+	void resetSearch();
+
+	void BFS(int s);
+
+	void DFS(int s);
+
+	void dfsUtil(int v);
+
+	bool hasPathTo(int v) { return marked[v]; }
+
+	stack<int> pathTo(int v);
+};
+
+Graph::Graph(int V) :
+		V(V), 
+		adjList(new unordered_set<int>[V])
+{
+	marked = new bool[V];
+	edgeTo = new int[V];
+	resetSearch();
+}
+
+void Graph::resetSearch()
+{
+	for (int i = 0; i < V; i++)
+	{
+		marked[i] = false;
+		edgeTo[i] = -1;
+	}
+}
+
+void Graph::addEdge(int src, int dest)
+{
+	adjList[src].insert(dest);
+	adjList[dest].insert(src);
+}
+
+void Graph::printGraph()
+{
+	for (int i = 0; i < V; i++)
+	{
+		unordered_set<int> adj = adjList[i];
+		cout << "Adjacency List of vertex " << i;
+		if (marked[i])
+			cout << "*\n";
+		else
+			cout << "\n";
+		for (auto& it : adj)
+		{
+			cout << it << " ";
+		}
+		cout << "\n";
+	}
+}
+
+void Graph::searchEdge(int src, int dest)
+{
+	auto it = adjList[src].find(dest);
+	if (it == adjList[src].end())
+		cout << "Edge not found!";
+	else
+		cout << "Edge found!";
+	cout << endl;
+}
+
+void Graph::BFS(int s)
+{
+	searchSrc = s;
+	cout << "BFS Traversal: \n";
+	resetSearch();
+	queue<int> q;
+	q.push(s);
+	marked[s] = true;
+	cout << s << " ";
+	while (!q.empty())
+	{
+		int v = q.front();
+		q.pop();
+		for (auto& it : adjList[v])
+		{
+			if (!marked[it])
+			{
+				q.push(it);
+				marked[it] = true;
+				edgeTo[it] = v;
+				cout << it << " ";
+			}
+		}
+	}
+	cout << endl;
+}
+
+void Graph::DFS(int s)
+{
+	searchSrc = s;
+	resetSearch();
+	cout << "DFS Traversal: \n";
+	dfsUtil(s);
+	cout << "\n";
+}
+
+void Graph::dfsUtil(int v)
+{
+	marked[v] = true;
+	cout << v << " ";
+	for (auto& it : adjList[v])
+	{
+		if (!marked[it]){
+			dfsUtil(it);
+			edgeTo[it] = v;
+		}
+	}
+}
+
+stack<int> Graph::pathTo(int v)
+{
+	stack<int> path;
+	if (!hasPathTo(v))
+		return path;
+	for (int x = v; x != searchSrc; x = edgeTo[x])
+		path.push(x);
+	path.push(searchSrc);
+	return path;
+}
+
+int main()
+{
+	int V = 7;
+	Graph g(V);
+	g.addEdge(0, 1);
+	g.addEdge(0, 4);
+	g.addEdge(1, 2);
+	g.addEdge(1, 3);
+	g.addEdge(1, 4);
+	g.addEdge(2, 3);
+	g.addEdge(3, 4);
+	g.addEdge(5, 6);
+	cout << "Before BFS: \n";
+	g.printGraph();
+
+	g.DFS(0);
+
+	stack<int> path = g.pathTo(2);
+	cout << "0 to 2(with DFS): \n";
+	while (!path.empty())
+	{
+		cout << path.top() << " ";
+		path.pop();	
+	}
+	cout <<  endl;
+	g.BFS(0);
+
+	path = g.pathTo(2);
+	cout << "0 to 2(with BFS): \n";
+	while (!path.empty())
+	{
+		cout << path.top() << " ";
+		path.pop();	
+	}
+
+	return 0;
+}
+
