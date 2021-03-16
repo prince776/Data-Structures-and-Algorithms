dfc144023f8377ad6b53865479dde6f798c68777
Prince Gupta <princemit776@gmail.com>
2021-03-16 20:21:39 +0530

    Improved DSU and kruskalMST

---
+++ DataStructures/DSU/DSU.h
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class DSU
+{
+private:
+    vector<int> parent, size;
+public:
+    DSU(int n)
+    {
+        parent = vector<int>(n);
+        size = vector<int>(n);
+        iota(begin(parent), end(parent), 0);
+    }
+    
+    int getParent(int x)
+    {
+        if (parent[x] == x) return x;
+        return parent[x] = getParent(parent[x]);
+    }
+
+    void join(int x, int y)
+    {
+        x = getParent(x);
+        y = getParent(y);
+        if (size[x] > size[y])
+            swap(x, y);
+        parent[x] = y;
+        size[y] += size[x];
+    }
+
+    int getSize(int x)
+    {
+        return size[x] = size[getParent(x)];
+    }
+};
---
+++ DataStructures/Graphs/kruskalMST.h
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "../DSU/DSU.h"
+
+class Edge
+{
+public:
+    int u, v, w;
+    Edge() = default;
+    Edge(int u, int v, int w) :
+        u(u), v(v), w(w) {};
+
+    bool operator<(Edge const& other)
+    {
+        return w < other.w;
+    }
+};
+
+auto kruskalMST(vector<Edge> &edges, int V)
+{
+    int cost = 0;
+    
+    DSU dsu(V);
+    sort(begin(edges), end(edges), [&](auto &x, auto &y) { return x.w < y.w; });
+
+    vector<Edge> tree;
+    for (const auto &[u, v, w] : edges)
+    {
+        if (dsu.getParent(u) != dsu.getParent(v))
+        {
+            cost += w;
+            tree.emplace_back(u, v, w);
+            dsu.join(u, v);
+        }
+    }
+
+    return make_pair(tree, cost);
+}
+
+auto makeGraphFromEdges(vector<Edge>& edges, int V)
+{
+    vector<vector<pair<int, int>>> adj(V);
+    for (auto &[u, v, w] : edges)
+    {
+        adj[u].emplace_back(v, w);
+        adj[v].emplace_back(u, w);
+    }
+    return adj;
+}
